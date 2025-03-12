28e91308e6932d7ed2d038d251b3121a2f20c467
sean-hyer <shyer2k@gmail.com>
2025-03-12 10:12:31 -0400

    Added a docstring to the apps.py file.

--- mysite/accounts/apps.py
+++ mysite/accounts/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """This is the default login system bundled with Django."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
